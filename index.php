214afc74f19de38d2ff4e7be30fb39147e024cb9
MarcoMpensabene <marcompensabene@gmail.com>
2024-06-28 15:10:35 +0200

    added session destroy and unset in logout.php , create a link in index that retunr to the login page

--- index.php
+++ index.php
@@ -7,4 +7,7 @@ include_once __DIR__ . "/login.php";
 
 <header>
     <?php echo "Welcome, " . $_SESSION['username']; ?></h1>
+    <div>
+        <a href="./logout.php">LOGOUT</a>
+    </div>
 </header>
--- logout.php
+++ logout.php
@@ -1,2 +1,9 @@
-<?php  
+<?php
 session_start();
+
+unset($_SESSION['username']);
+session_destroy();
+
+header('Location: login.php');
+
+?>
